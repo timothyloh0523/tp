260d2112fe2aecf5fca0bb0b3b042b0822ff0aac
timothyloh0523 <timothyloh0523@ymail.com>
2025-03-19 19:19:13 +0800

    Added "change x" function, to change betting amount to x; Changed case-switch function to take in first word of input instead of full input

--- src/main/java/seedu/coinflip/Coinflip.java
+++ src/main/java/seedu/coinflip/Coinflip.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Coinflip {
     private static int balance = 500;
+    private static int betAmount = 20;
 
     /**
      * Main entry-point for the java.seedu.coinflip.Coinflip application.
@@ -11,20 +12,33 @@ public class Coinflip {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-
         System.out.println("Welcome to Coinflip!");
 
-        while (true)
+        boolean isExit = false;
+        while (!isExit)
         {
             String input = in.nextLine();
-            switch (input) {
-            case "check balance":
-                System.out.println("Your remaining balance is: " + balance);
+            String[] words = input.split("\\s+");
+            switch (words[0]) {
+            case "check":
+                if (words.length > 1 && words[1].equals("balance")) {
+                    System.out.println("Your remaining balance is: " + balance);
+                }
+                break;
+            case "change":
+                try {
+                    betAmount = Integer.parseInt(words[1]);
+                    System.out.println("Changed your bet amount to " + betAmount + "!");
+                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Please provide a valid bet amount!");
+                }
                 break;
             case "exit":
                 System.out.println("Thank you for using Coinflip. Goodbye!");
+                isExit = true;
                 break;
             default:
+                System.out.println("Invalid command!");
                 break;
             }
         }
