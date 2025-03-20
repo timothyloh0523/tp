f4085096ab4f8ff80de04142a55d835d2a777fb6
HTY2003 <tengyi.maker@gmail.com>
2025-03-21 02:33:01 +0800

    Add abstraction for functions, handle more incorrect user input

--- src/main/java/seedu/coinflip/utils/exceptions/CoinFlipException.java
+++ src/main/java/seedu/coinflip/utils/exceptions/CoinFlipException.java
@@ -5,7 +5,10 @@ public class CoinFlipException extends Exception {
     public static final String FLIP_INVALID_FORMAT = "Invalid command format! " +
             "Please follow this format: flip <heads>/<tails>";
 
-    public static final String BET_AMOUNT_INVALID_FORMAT = "Please provide a valid bet amount!";
+    public static final String BET_AMOUNT_INVALID_FORMAT = "Please provide a valid positive number as your bet amount!";
+
+    public static final String CHECK_INVALID_FORMAT = "Invalid command format! " +
+            "Please follow this format: check <bet>/<balance>.";
 
     public String message = "";
 
