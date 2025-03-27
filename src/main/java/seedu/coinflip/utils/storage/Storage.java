ec6e2e785d88ad3d8c0b5652dd2522adca355b28
HTY2003 <tengyi.maker@gmail.com>
2025-03-28 04:22:39 +0800

    Remove wildcard import

--- src/main/java/seedu/coinflip/utils/storage/Storage.java
+++ src/main/java/seedu/coinflip/utils/storage/Storage.java
@@ -5,7 +5,11 @@ import seedu.coinflip.utils.logger.CoinflipLogger;
 import seedu.coinflip.utils.printer.Printer;
 import seedu.coinflip.utils.userdata.UserData;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
