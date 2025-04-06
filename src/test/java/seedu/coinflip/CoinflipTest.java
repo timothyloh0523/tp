104db6cbdee7b28cb8344cd156b3c3e4a38003c3
CRL006 <chongruolin@gmail.com>
2025-04-06 17:48:05 +0800

    Edit testcase

--- src/test/java/seedu/coinflip/CoinflipTest.java
+++ src/test/java/seedu/coinflip/CoinflipTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
 
 class CoinflipTest {
